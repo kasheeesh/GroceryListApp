2a60fd64cc7f32f0ca9d2d9d21287d3e87b52677
kasheesh <kashishbhurani@gmail.com>
2023-09-25 17:32:17 +0530

    Added realtime databse using firebase

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const appSettings = {
+    databaseURL: "https://playground-a434d-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 
 addButtonEl.addEventListener("click", function() {
-    console.log(inputFieldEl.value)
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    console.log(inputValue)
 })
